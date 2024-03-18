4f536eb539e39b51f374649626e850f275c3061d
abbas <abbas>
2024-03-18 14:11:22 -0400

    add usersdb

--- final_project/stores/booksdb.js
+++ final_project/stores/booksdb.js
@@ -1,4 +1,4 @@
-let books = {
+const books = {
       1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
@@ -9,6 +9,10 @@ let books = {
       8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
       9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
       10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+};
 
-module.exports=books;
+const users={  
+};
+
+
+module.exports={books, users};
