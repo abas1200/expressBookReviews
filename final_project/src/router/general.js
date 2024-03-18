e76bf1d6631914564f44cbc3ee92f868a2d24c21
abbas <abbas>
2024-03-18 19:59:08 -0400

    added get review

--- final_project/src/router/general.js
+++ final_project/src/router/general.js
@@ -3,6 +3,8 @@ const {
   getBookList,
   getBooksByISBN,
   getBooksByAuthor,
+  getBooksByTitle,
+  getReview,
 } = require("../services/bookService");
 //let isValid = require("./auth_users.js").isValid;
 //let users = require("./auth_users.js").users;
@@ -23,15 +25,9 @@ public_users.get("/isbn/:isbn", getBooksByISBN);
 public_users.get("/author/:author", getBooksByAuthor);
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+public_users.get("/title/:title", getBooksByTitle);
 
 //  Get book review
-public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+public_users.get("/review/:isbn", getReview);
 
 module.exports.general = public_users;
--- final_project/src/services/bookService.js
+++ final_project/src/services/bookService.js
@@ -5,17 +5,33 @@ const getBookList = (req, res) => {
 };
 
 const getBooksByISBN = (req, res) => {
-  const book = bookStore[req.params.isbn] ?? {};
+  const book = bookStore[req.params.isbn] ?? "Book Not Found!";
   res.json(book);
 };
 
 const getBooksByAuthor = (req, res) => {
-  const books = Object.values(bookStore).filter(b=>b.author == req.params.author);
+  const books = Object.values(bookStore).filter(
+    (b) => b.author == req.params.author
+  );
   res.json(books);
+};
+
+const getBooksByTitle = (req, res) => {
+  const books = Object.values(bookStore).filter(
+    (b) => b.title == req.params.title
+  );
+  res.json(books);
+};
+
+const getReview = (req, res) => {
+  const reviews = bookStore[req.params.isbn]?.reviews ?? "Book Not Found!";
+  res.json(reviews);
 };
 
 module.exports = {
   getBookList,
   getBooksByISBN,
   getBooksByAuthor,
+  getBooksByTitle,
+  getReview
 };
