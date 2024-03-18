6b46c54ea214e7b72c8b4ea8f54034035af34ecf
abbas <abbas>
2024-03-18 19:52:22 -0400

    added getbooks methods

--- final_project/src/router/general.js
+++ final_project/src/router/general.js
@@ -1,40 +1,37 @@
-const express = require('express');
-const { getBookList } = require('../services/bookService');
+const express = require("express");
+const {
+  getBookList,
+  getBooksByISBN,
+  getBooksByAuthor,
+} = require("../services/bookService");
 //let isValid = require("./auth_users.js").isValid;
 //let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',getBookList);
+public_users.get("/", getBookList);
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get("/isbn/:isbn", getBooksByISBN);
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get("/author/:author", getBooksByAuthor);
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.general = public_users;
--- final_project/src/services/bookService.js
+++ final_project/src/services/bookService.js
@@ -1,10 +1,21 @@
 const bookStore = require("../stores/booksdb.js");
 
-const getBookList = ((req, res) => {
-    res.json(bookStore);
-})
+const getBookList = (req, res) => {
+  res.json(bookStore);
+};
 
- 
+const getBooksByISBN = (req, res) => {
+  const book = bookStore[req.params.isbn] ?? {};
+  res.json(book);
+};
+
+const getBooksByAuthor = (req, res) => {
+  const books = Object.values(bookStore).filter(b=>b.author == req.params.author);
+  res.json(books);
+};
+
 module.exports = {
-    getBookList,
-}
+  getBookList,
+  getBooksByISBN,
+  getBooksByAuthor,
+};
